9bbcbd59c15f82fd437e5a5c8f4857189abef29f
JoeCaudill <caudillw@sou.edu>
2012-10-15 13:44:14 -0700

    Update src/UAphp.php

--- src/UAphp.php
+++ src/UAphp.php
@@ -5,3 +5,15 @@ echo "Hello Total and Current Value!";
 <?php
 echo "Hello User Validation!";
 ?>
+
+<?php
+echo "Hello buyStock!!!";
+?>
+
+<?php
+echo "Hello sellStock!!!";
+?>
+
+<?php
+echo "Hello deposit!!!";
+?>
