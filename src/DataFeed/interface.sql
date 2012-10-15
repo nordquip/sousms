28dac3812c8d9a33715ec6633aa24a8610bd039b
shaunwolff <shaun.wolff@gmail.com>
2012-10-15 16:47:06 -0700

    Update src/DataFeed/interface.sql

--- src/DataFeed/interface.sql
+++ src/DataFeed/interface.sql
@@ -1,7 +1,7 @@
 /* enterTickerData(bestAskPrice, bestAskQty, bestBidPrice, bestBidQty, close, high, lastSale, low, netChg, open, pcl, vol, date, pctChg, symbol, time)
 
 This Procedure must:
-1. Store all variables in corresponding fields into appropriate table
+1. Store all variables into corresponding fields on the appropriate table
 */
 
 
