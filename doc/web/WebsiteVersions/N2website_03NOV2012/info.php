4a81e91cd8f5913136da175321d4f5e14626e16f
johnrekow <johnrekow@ukinbox.com>
2012-11-04 02:41:56 +0000

    Site Update 03NOV http://webpages.sou.edu/~rekowj/

---
+++ doc/web/WebsiteVersions/N2website_03NOV2012/info.php
@@ -0,0 +1,54 @@
+﻿
+
+
+<? phpinfo(); ?>
+
+
+
+
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+<META HTTP-EQUIV="Expires" CONTENT="-1">
+<META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
+<title>PHP Info Page</title>
+<META NAME="ROBOTS" CONTENT="NONE, NOARCHIVE">
+<META NAME="GOOGLEBOT" CONTENT="NOARCHIVE">
+<meta http-equiv="Expires" content="Tue, 04 Dec 1997 21:29:02 GMT">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Cache-Control" content="no-cache">
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+<script type="text/javascript" src="jquery/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="jquery/jquery.gallerax-0.2.js"></script>
+<link rel="stylesheet" type="text/css" href="2leveltab.css" />
+<script type="text/javascript" src="2leveltab.js"></script>
+<style type="text/css">
+@import "gallery.css";
+</style>
+
+</head>
+
+<body>
+
+
+
+<br /><p>
+		 PHP Info Page.<br />
+		
+<br />
+		 <a href="info.php?=PHPE9568F34-D428-11d2-A769-00AA001ACF42">Maybe this experimental link works, or not.</a><br />
+		
+<br />
+		<br /></p>
+
+</body>
+
+</html>
