7b881b05418db421040ca7eae7c894998ec29621
JesseAllred <allredj@sou.edu>
2012-11-06 00:10:04 -0800

    Update doc/web/WebServicePHPs/tradehistory.php

--- doc/web/WebServicePHPs/tradehistory.php
+++ doc/web/WebServicePHPs/tradehistory.php
@@ -2,7 +2,25 @@
 <head><title>Trade History</title></head>
 <body>
 <h2>My Trade History</h2>
-<?php   //placeholder text
+<?php  
+/********************************************
+* tradehistory.php
+* by Jesse Allred
+********************************************/
+
+include("WSRequestManager.class.php");
+include("login.include.php");
+
+class TradeHistory
+{
+   public $userName,$numShares,$symbol;
+};
+function GetTradeHistory
+{
+   $token = $POST['token'];
+   $userID = mysql_query("call getUserID(token)");
+   //don't know the methods yet
+};
    echo 'trade history php called';
 ?>
 </body>
