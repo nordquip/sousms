9ea86953281f27ff240f6a07ffaec6917113a738
L Peery <peeryl@sou.edu>
2012-11-05 20:37:30 -0800

    add stored procedures file

---
+++ src/userAccountsStoredProcedures.sql
@@ -0,0 +1,32 @@
+/*
+	UA stored procedures
+*/
+/*
+This Procedure must:
+1. Check to see if the email entered by the user exists in the database
+2. Return true of false
+*/
+drop procedure if exists doesEmailExist;
+delimiter //
+create procedure doesEmailExist(
+	IN email varchar(40)
+//
+delimiter ;
+
+
+/*
+This Prodecure must:
+1. Insert temporary password in the database
+2. Return true (inserted) or false (failed)
+*/
+drop procedure if exists insertTempPassword;
+delimiter//
+create procedure insertTempPassword;
+	IN password varchar (40)
+
+begin
+insert into User values User(password);
+select * from User;
+end;
+//
+delimiter;
