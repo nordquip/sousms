0f5bd6ae559792961bfc9f0ad70531f8fb86a80a
Scott Williams <csp020202@hotmail.com>
2012-11-12 13:09:50 -0800

    Stored procedure
    doesnt compile

--- src/database/storedProcs.sql
+++ src/database/storedProcs.sql
@@ -69,3 +69,48 @@ BEGIN
 		SET SUCCESS = '1';
 	END IF
 END;
+DELIMITTER //
+
+CREATE PROCEDURE sell()
+
+IN UID int,
+IN SYM varchar(5),
+IN NUM int,
+IN PRC float(10,4),
+
+BEGIN
+	--sets success. 
+	SET yes = 1;
+	SET complete = 1;
+	
+	--checks if user has stock to sell.
+	SELECT symbol 
+	FROM portfolio
+		IF (symbol == SYM)THEN
+			yes = 0;
+		ELSE
+			yes = 1;
+	WHERE userID = UID;
+			
+	--checks the curent price of stock.
+	SELECT LastSale
+	FROM feed;
+	
+	--removes the stock from portfolio after sold.
+	DELETE FROM portfolio
+	WHERE symbol == SYM
+	AND userID = UID;
+	
+	--adds amount of sale to balance.
+	UPDATE cash
+	SET balance = LastSale + PRC
+	WHERE userID = UID;
+
+	complete = 0;
+END //
+
+DELIMITTER;
+
+
+
+
