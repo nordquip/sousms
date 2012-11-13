9e08c25283693fd77779e3ea835c4ef2f6ee188b
Jeff Miller <scriptar@gmail.com>
2012-11-13 00:37:28 -0800

    TE tables and SPs... not yet finished

--- src/te/OpenOrderHistory.sql
+++ src/te/OpenOrderHistory.sql
@@ -7,25 +7,28 @@ DROP TRIGGER IF EXISTS LogOrders;
 DROP TABLE IF EXISTS OpenOrdersHistory;
 
 CREATE TABLE IF NOT EXISTS OpenOrdersHistory (
+	`historyID`	int(11)		NOT NULL AUTO_INCREMENT,
 	`orderID`	int(11)		NOT NULL,
 	`userID`	int(11)		NOT NULL,
-	`stockSymbol`	varchar(50)	NOT NULL,
+	`symID`		int(11)		NOT NULL,
 	`shares`	int(11)		NOT NULL,
-	`orderType`		int(11)		NOT NULL,
+	`orderType`	int(11)		NOT NULL,
 	`price`		numeric(13,2)	NULL,
 	`requestTime`	datetime	NOT NULL,
-	PRIMARY KEY (`orderID`),
+	PRIMARY KEY (`historyID`),
 	CONSTRAINT `openordershistory_ordertype_FK` FOREIGN KEY (`orderType`)
-		REFERENCES `OrderTypes` (`typeID`)
+		REFERENCES `OrderTypes` (`typeID`),
+	CONSTRAINT `openordershistory_symbol_FK` FOREIGN KEY (`symID`)
+		REFERENCES `Symbol` (`symID`)
 ) ENGINE = InnoDB;
 
-delimiter |
+delimiter //
 CREATE TRIGGER LogOrders AFTER INSERT ON OpenOrders
 	FOR EACH ROW
 	BEGIN
 		INSERT INTO OpenOrdersHistory
-		(orderID, userID, stockSymbol, shares, orderType, price, requestTime)
+		(orderID, userID, symID, shares, orderType, price, requestTime)
 		VALUES
-		(NEW.orderID, NEW.userID, NEW.stockSymbol, NEW.shares, NEW.orderType, NEW.price, NEW.requestTime);
+		(NEW.orderID, NEW.userID, NEW.symID, NEW.shares, NEW.orderType, NEW.price, NEW.requestTime);
 	END;
-|
+//
--- src/te/TradeEngineStoredProcedures.sql
+++ src/te/TradeEngineStoredProcedures.sql
@@ -3,66 +3,105 @@
  * the Trade Engine team.
  */
 
+-- sp_symbolToSymID
+-- Converts a symbol to a symbol ID.
+DROP PROCEDURE IF EXISTS sp_symbolToSymID;
+DELIMITER //
+CREATE PROCEDURE `sp_symbolToSymID` (
+	stockSymbol varchar(8)
+)
+BEGIN
+	SELECT symID FROM Symbol WHERE Symbol.symbol = stockSymbol;
+END;
+//
+
+DELIMITER ;
 
 -- sp_insertSell
-delimiter //
-CREATE PROCEDURE sp_insertSell(
+DROP PROCEDURE IF EXISTS sp_insertSell;
+DELIMITER //
+CREATE PROCEDURE `sp_insertSell` (
 	IN userID_in  int(11),
-	IN stockSymbol_in  varchar(50),
+	IN symbolID_in  int(11),
 	IN shares_in int(11),
 	IN price_in  numeric(13,2)
-	)
-	
+)
 BEGIN
-
-INSERT INTO OpenOrders (userID, stockSymbol, shares, orderType, price, requestTime) 
-	VALUES (userID_in, stockSymbol_in, shares_in,
-	(SELECT `typeID` FROM OrderTypes WHERE `description` LIKE 'Sell'), 
-	price_in, NOW());
+	INSERT INTO OpenOrders (userID, symID, shares, orderType, price, requestTime) 
+		VALUES (userID_in, symbolID_in, shares_in,
+		(SELECT `typeID` FROM OrderTypes WHERE `description` LIKE 'Sell'), 
+		price_in, NOW());
+	SELECT 'Your trade has been queued' AS statusmsg;
 END;
 //
 
-delimiter ;
+DELIMITER ;
 
 -- sp_getShareBalance
+-- this stored procedure still needs some tweaking - it does not successfully return the result set
 DROP PROCEDURE IF EXISTS sp_getShareBalance;
-
-delimiter //
-CREATE PROCEDURE sp_getShareBalance(
+DELIMITER //
+CREATE PROCEDURE `sp_getShareBalance` (
 	IN userID_in  int(11),
-	IN stockSymbol_in  varchar(50)
-	)
-	
+	IN stockSymbol_in  varchar(8)
+)
 BEGIN
-
 	SELECT `Shares` FROM Portfolio  
-	WHERE `UserID` = userID_in 
-	AND `SymID` IN(
-		SELECT `SymID` FROM Symbol 
-		WHERE  `Symbol` = stockSymbol_in);
+		WHERE `UserID` = userID_in 
+		AND `SymID` IN (
+			SELECT `SymID` FROM Symbol 
+			WHERE  `Symbol` = stockSymbol_in
+		);
 END;
 //
 
-delimiter ;
+DELIMITER ;
 
+-- sp_getAllOpenOrders
+DROP PROCEDURE IF EXISTS sp_getAllOpenOrders;
+DELIMITER //
+CREATE PROCEDURE `sp_getAllOpenOrders` ()
+BEGIN
+	-- Returns all records of the OpenOrders table
+	SELECT  openorders.orderID,
+			openorders.userID, 
+			openorders.symID, 
+			symbol.symbol,
+			openorders.shares,
+			openorders.orderType,
+			ordertypes.description AS typedesc,
+			openorders.price
+		FROM openorders
+			JOIN symbol ON openorders.symID = symbol.symID
+			JOIN ordertypes ON openorders.orderType = ordertypes.typeid
+		ORDER BY orderID;
+END;
+//
 
--- sp_getAllOpenOrders	
-sp_getAllOpenOrders()
-	-- Returns userID, stockSymbol, shares, orderType, price from all records of the OpenOrders table
+DELIMITER ;
 
-	
 -- sp_deleteOpenOrder
-sp_deleteOpenOrder(orderID)
-	-- deletes the record corresponding to orderID from OpenOrders
-	
-	
--- sp_getUserIdFromToken	
-DELIMITER $$
-CREATE PROCEDURE `sp_getUserIdFromToken`(token char(32))
+-- deletes the record corresponding to orderID from OpenOrders
+DROP PROCEDURE IF EXISTS sp_deleteOpenOrder;
+DELIMITER //
+CREATE PROCEDURE `sp_deleteOpenOrder` (orderID INT)
+BEGIN
+	DELETE FROM openorders WHERE openorders.orderID = orderID;
+END;
+//
+
+DELIMITER ;
+
+-- sp_getUserIdFromToken
+DROP PROCEDURE IF EXISTS sp_getUserIdFromToken;
+DELIMITER //
+CREATE PROCEDURE `sp_getUserIdFromToken`(
+	token char(32)
+)
 BEGIN
 	DECLARE lnUserID INT;
-	DECLARE ldExpireTime DATETIME;
-	DECLARE loginCursor CURSOR FOR SELECT UserID, ExpTime FROM logins
+	DECLARE ldExpireTime TIMESTAMP;
+	DECLARE loginCursor CURSOR FOR SELECT UserID, EndTS FROM login
 		WHERE logins.token = token;
 	DECLARE EXIT HANDLER FOR NOT FOUND BEGIN
 		SELECT -1 AS userid, 'The login was not found' AS statusmsg;
@@ -76,47 +115,93 @@ BEGIN
 		SELECT -1 AS userid, 'The login expired' AS statusmsg; 
 	END IF;
 END;
-$$
+//
 
 DELIMITER ;
 
-
--- sp_marketBuy
-/*Nov 5, 2012 Created By Jeff Karmy, with Jeff Miller's help
-*/
-drop procedure if exists sp_marketBuy;
-delimiter //
-create procedure sp_marketBuy(
-	IN stockSymbol char(10),
+-- sp_insertBuy
+-- Nov 5, 2012 Created By Jeff Karmy, with Jeff Miller's help
+DROP PROCEDURE IF EXISTS sp_insertBuy;
+DELIMITER //
+CREATE PROCEDURE `sp_insertBuy` (
+	IN userID int,
+	IN symbolID int,
 	IN shares int,
-	IN userID char(30)
-	)
-	
-begin 
-insert into OpenOrders (userID, stockSymbol, shares, orderType, requestTime)
-values (userID, stockSymbol, shares, (select typeID from OrderTypes where description like 'Buy'), now());
-select 'Your trade has been queued';
-end;
+	IN price numeric(13,2)
+)
+BEGIN
+	INSERT INTO OpenOrders (userID, symID, shares, orderType, price, requestTime)
+	VALUES (userID, symbolID, shares, (SELECT typeID FROM OrderTypes WHERE description LIKE 'Buy'), price, NOW());
+	SELECT 'Your trade has been queued' AS statusmsg;
+END;
 //
 
-delimiter ; 
+DELIMITER ;
 
 -- sp_getCash
-sp_getCash(userID)
-	-- returns user's cash
+-- returns user's cash
+DROP PROCEDURE IF EXISTS sp_getCash;
+DELIMITER //
+CREATE PROCEDURE `sp_getCash` (
+	userID INT
+)
+BEGIN
+	SELECT SUM(balance) AS totalbalance FROM cash WHERE cash.userID = userID;
+END;
+//
+
+DELIMITER ;
 
 -- sp_getPrice
-sp_getPrice(symID)
-	-- returns latest price
+-- returns latest stock price
+DROP PROCEDURE IF EXISTS sp_getPrice;
+DELIMITER //
+CREATE PROCEDURE `sp_getPrice` (
+	symID INT
+)
+BEGIN
+	SELECT bestAskPrice AS price
+	FROM feed
+		JOIN symbol ON symbol.symbol = feed.symbol
+	WHERE symbol.symID = symID
+	ORDER BY feed.date DESC,
+		feed.time DESC
+	LIMIT 1;
+END;
+//
 
+DELIMITER ;
 
--- sp_decAvailCash
-sp_decAvailCash(userID, cashToDec)
-	-- if.. using a separate "available cash" column on the user table
-	-- then.. decrements user's available balance by 'cashToDec'
+-- sp_buy
+-- executes "buy" behavior for given order ID:
+--   inserts negative balance (-1 * current price * number of shares) into "cash" table for given user
+--   inserts into stock holdings (userID, symID) if not exists
+--   updates stock holdings (shares, datemodified) in "portfolio" table for (userID, symID)
+--   deletes order for given order ID
+--   rolls back changes if any failures
+--   reports success status
+DROP PROCEDURE IF EXISTS sp_buy;
+DELIMITER //
+CREATE PROCEDURE `sp_buy` (
+	openOrderID INT
+)
+BEGIN
+	
+END;
+//
 
+DELIMITER ;
 
--- sp_exeBuy
-sp_exeBuy(userID, symID, numShares, stockPrice, cashToDec)	
-	-- decrements user's "actual cash" balance on the 'user' table
-	-- makes record of new stock holding (userID, symID, numShares, stockPrice) on a "stock holdings" table
+-- sp_sell
+DROP PROCEDURE IF EXISTS sp_buy;
+DELIMITER //
+CREATE PROCEDURE `sp_buy` (
+	openOrderID INT
+)
+BEGIN
+	
+END;
+//
+
+DELIMITER ;
+
