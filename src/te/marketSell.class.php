cb2d6b8db8736d002ffbe961f0a46a392967c412
Brittany Dighton <dightonb@sou.edu>
2012-11-12 21:27:44 -0800

    finalized MarketSell.class.php

--- src/te/marketSell.class.php
+++ src/te/marketSell.class.php
@@ -9,59 +9,62 @@
  
  class MarketSell 
  {	
-	private $conn;
-	private $mssg;
-	
-	public function sell($conn_in, $userID, $stockSymbol, $numShares) 
+ 
+	public function sell($conn, $user, $symbol, $shares) 
 	{	
-		$this->conn = $conn_in;
-		$user = $userID;
-		$symbol = $stockSymbol;
-		$shares = $numShares;
 		$price = NULL;
+		$shareBal = 0;
+		$mssg = "";
 	
 		try 
 		{			
-			$stmt = $this->conn->prepare("CALL sp_getShareBalance(?,?)");	
-			$stmt->bindParam(1, $user, PDO::PARAM_INT);
-			$stmt->bindParam(2, $symbol, PDO::PARAM_STR, 12);
-			$stmt->execute();				
-				
-			$stmt->bindColumn(1, $shareBal);
-			$stmt->fetch(PDO::FETCH_BOUND);	
-			
+			$stmt = $conn->prepare("CALL sp_getShareBalance(?,?)");	
+			$stmt->execute(array($user, $symbol));				
+			if($stmt->rowCount() > 0)
+			{
+				$stmt->bindColumn(1, $shareBal);
+				$stmt->fetch(PDO::FETCH_BOUND);	
+			}
+			else
+			{
+				$mssg .= "Unable to verify share balance.";
+			}
 		}		
 	 
 		catch (PDOException $e) 
 		{			
-			$this->mssg = "Error: " . $e->getMessage();	
+			$mssg .= "Error: " . $e->getMessage();	
 		}
 	
 		if ($shareBal >= $shares)
 		{
 			try
 			{
-				$stmt = $this->conn->prepare("CALL sp_insertSell(?,?,?,?)");
-				$stmt->bindParam(1, $user, PDO::PARAM_INT);
-				$stmt->bindParam(2, $symbol, PDO::PARAM_STR, 12);
-				$stmt->bindParam(3, $shares, PDO::PARAM_INT);
-				$stmt->bindParam(4, $price);
-				$stmt->execute();
-			
-				$this->mssg = "Your order was successfully queued.";
+				$stmt = $conn->prepare("CALL sp_insertSell(?,?,?,?)");
+				$stmt->execute(array($user, $symbol, $shares, $price));
+				$stmt->bindColumn(1, $returnCode);
+				$stmt->fetch(PDO::FETCH_BOUND);
+				
+				if ($stmt->rowCount() > 0)
+				{
+					$mssg .= "Your order was successfully queued.";
+				}
+				else
+				{
+				$mssg .= "Error:  Order not successfully queued.";
+				}
 			}
 		
 			catch (PDOException $e) 
 			{			
-				$this->mssg = "Error: " . $e->getMessage();	
+				$mssg .= "Error: " . $e->getMessage();	
 			}
 		}
 		else
 		{
-			$this->mssg = "Error:  Insufficient holdings of ". $symbol. " to sell ". $shares. " shares.";
+			$mssg .= "Error:  Insufficient holdings of $symbol to sell $shares shares.";
 		}
-
-		return ($this->mssg);
+		return ($mssg);
 		
 	}
 }
