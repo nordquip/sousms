75ca98d42c830a58dea1e071f8ce93d1db01d3bf
Terak Raines <Terak_r@hotmail.com>
2012-11-14 17:36:26 -0800

    profilemaster.phpTest1.8

--- src/mobile/html/profilemaster.php
+++ src/mobile/html/profilemaster.php
@@ -1,29 +1,27 @@
-<!--<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
--->
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN""http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
 <?php
 
 ?>
 <html>
 <head>
+	
 	<script type="text/javascript">
-	<?php
-	int $availableBal;
-	int $currentHoldings;
-	?>
-	function getBalance();
+	//int $availableBal;
+	//int $currentHoldings;
+	function int getBalance();
 	{
 		
-		$availableBal = 500;
+		int $availableBal = 500;
 		//alert("you have " + $availableBal + " dollars to spend");
 		return $availableBal;
 	}
 	
-	function getHoldings()
+	function void getHoldings()
 	{
 		int $MSFT_Shares = 5;
 		int $MFST_Price= 100;
-		$currentHoldings = $MSFT_Shares * $MSFT_Price;
+		int $currentHoldings = $MSFT_Shares * $MSFT_Price;
 		return $currentHoldings;
 	}
 	
@@ -39,9 +37,9 @@ html {text-align:center}
 	<div id="totalMoney" align="center" style= "border:1px solid grey">
 		
 			
-			You have a current balance of: $ <?php echo '<script>getBalance();</script>' ?>
+			You have a current balance of: $ <?php echo'<script> getBalance();</script>' ?>
 			<br>
-			The total worth of your current stocks is: $ <?php echo '<script>getHoldings();</script>' ?>
+			The total worth of your current stocks is: $ <?php echo '<script> getHoldings();</script>' ?>
 			
 	</div>
 
