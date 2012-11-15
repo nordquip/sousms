2bd0eb947359b459bf72a8dc66a8f6369d256d83
Terak Raines <Terak_r@hotmail.com>
2012-11-14 17:09:31 -0800

    profilemaster.phpTest1.6

--- src/mobile/html/profilemaster.php
+++ src/mobile/html/profilemaster.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <?php
+
+?>
+<html>
+<head>
+	<?php
 	<script type="text/javascript">
 	int $availableBal;
 	int $currentHoldings;
@@ -21,11 +26,6 @@
 	}
 	
 	</script>
-?>
-<html>
-<head>
-	<?php
-
 	?>
 	<style>
 html {text-align:center}
