1b21912229176b0aceef5b3e63a34298e78c3378
Sean Ewing <seandabom08@gmail.com>
2012-11-14 17:03:37 -0800

    Mobile CSS

--- src/mobile/html/index.php
+++ src/mobile/html/index.php
@@ -1,7 +1,16 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML5 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<title>SOUSMS Home</title>
+	<script type="text/css" src="css/style.css"></script>
+	<script type="text/css">
+	div.login{
+		max-height:200px;
+		max-width:450px;
+		min-height:110px;
+		min-width:300px;
+	}
+	</script>
 	<script type="text/javascript">
 		function Authenticate()
 		{	
@@ -14,14 +23,14 @@
 </head>
 <body>
 	
-	<div id="main">
-		<h1><center>Login to SOU SMS<center></h1>
+	<div class="main">
+		<h1><center>Login to SOU SMS</center></h1>
 		
-		<div id="login" align="center">
+		<div class="login">
 			
 			<form autocomplete="on"> <!--action="authenticationhandler.php" method="post"-->
-				<span id="user"> Username:</span><input type="text" id="username" value=""/><br />
-				<span id="pass"> Password:</span><input type="Password" id="password" value=""/><br />
+				<span id="user"> Username:</span><br /><input type="text" id="username" value=""/><br />
+				<span id="pass"> Password:</span><br /><input type="Password" id="password" value=""/><br />
 				<input type="button" id="submit" value="Submit" onclick="Authenticate()"/><!-- Authenticate() will be replaced with the authentication call to the server-->
 			</form>
 			
