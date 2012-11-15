157daa43e74f56c407b9933b3478351f2ca00e06
terakRaines <rainest@sou.edu>
2012-11-14 16:38:32 -0800

    Update src/mobile/html/profilemaster.php

--- src/mobile/html/profilemaster.php
+++ src/mobile/html/profilemaster.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <?php
-
-//}xmlns="http://www.w3.org/1999/xhtml"
-
-
-?>
-<html>
-<head>
-	<?php
 	<script type="text/javascript">
 	int $availableBal;
 	int $currentHoldings;
@@ -29,6 +21,11 @@
 	}
 	
 	</script>
+?>
+<html>
+<head>
+	<?php
+
 	?>
 	<style>
 html {text-align:center}
