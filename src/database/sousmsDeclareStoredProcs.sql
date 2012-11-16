d70d31bb60a1bc133803cdda4fabe34a15f74647
Ryan Dempsey <Ryan5732@gmail.com>
2012-11-15 19:14:08 -0800

    Update src/database/sousmsDeclareStoredProcs.sql

--- src/database/sousmsDeclareStoredProcs.sql
+++ src/database/sousmsDeclareStoredProcs.sql
@@ -2,7 +2,7 @@
 	sousms Stored Procedure interfaces with their implementations
 	Pete Nordquist 121019
 	
-	Note: Be sure to drop a procedure here if it already exists -- we will be running this file on a populated server. If you do not drop the procedure, errors will occur. --Ryan5732 
+	Note: Be sure to drop procedures if they already exist -- we will be running this on a populated server. If you do not drop the procedure, errors will occur. --Ryan5732 
 	
 	Note: Do not delete or rename this file... it is being used by the build script. --Ryan5732
 */
