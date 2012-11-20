308e9c0d96e219b8b89240099d3b51721ecf34c3
JesseAllred <allredj@sou.edu>
2012-11-19 23:17:32 -0800

    Update src/web/tradehistory.php

--- src/web/tradehistory.php
+++ src/web/tradehistory.php
@@ -1,18 +1,11 @@
-
 <?php
-/******************************************************************
-* trade.php
-* By: Jeff Miller (millerj3@students.sou.edu), 2012-10-24
-* Description: Example of a page that requires login to access.
-******************************************************************/
-
 include("login.include.php");
+include $_SERVER['DOCUMENT_ROOT'] . '/webServiceCaller.include.php';
 ?>
 
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
@@ -20,7 +13,7 @@ include("login.include.php");
 <META HTTP-EQUIV="Pragma" CONTENT="no-cache">
 <META HTTP-EQUIV="Expires" CONTENT="-1">
 <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
-<title>N2 Trading</title>
+<title>Trade History</title>
 <META NAME="ROBOTS" CONTENT="NONE, NOARCHIVE">
 <META NAME="GOOGLEBOT" CONTENT="NOARCHIVE">
 <meta http-equiv="Expires" content="Tue, 04 Dec 1997 21:29:02 GMT">
@@ -35,61 +28,36 @@ include("login.include.php");
 @import "gallery.css";
 </style>
 </head>
+
 <body>
-		<center><img src="images/banner.jpg" width="810" height="160" alt="NASDAQ Ninja logo" 
+<a name="glosstop"></a> <center><img src="images/banner.jpg" width="810" height="160" alt="NASDAQ Ninja logo" /></center>
+<!-- end #header -->
 
-/></center>
-	<!-- end #header -->
-    <ul id="maintab" class="basictab">
-			<li rel="homesubs"><a href="home.php"><img 
-
-src="images/Home.jpg" onmouseover=this.src="images/Homeselected.jpg" 
-
-onmouseout=this.src="images/Home.jpg" /></a></li>
-			<li class="selected" rel="tradesubs"><a href="trading.php"><img 
-
-src="images/Tradeselected.jpg" onmouseover=this.src="images/Tradeselected.jpg" 
-
-onmouseout=this.src="images/Tradeselected.jpg" /></a></li>
-            <li rel="looksubs"><a href="lookup.php"><img src="images/Lookup.jpg" 
-
-onmouseover=this.src="images/Lookupselected.jpg" onmouseout=this.src="images/Lookup.jpg" /></a></li>
-			<li rel="setsubs"><a href="settings.php"><img 
-
-src="images/Settings.jpg" onmouseover=this.src="images/Settingsselected.jpg" 
-
-onmouseout=this.src="images/Settings.jpg" /></a></li>
-            <li rel="helpsubs"><a href="help.php"><img src="images/Help.jpg" 
-
-onmouseover=this.src="images/Helpselected.jpg" onmouseout=this.src="images/Help.jpg" /></a></li>
-            <li rel="aboutsubs"><a href="about.php"><img src="images/About_Us.jpg" 
-
-onmouseover=this.src="images/About_Usselected.jpg" onmouseout=this.src="images/About_Us.jpg" 
-
-/></a></li>
-            <li rel="contactsubs"><a href="contact.php"><img src="images/Contact_Us.jpg" 
-
-onmouseover=this.src="images/Contact_Usselected.jpg" onmouseout=this.src="images/Contact_Us.jpg" 
-
-/></a></li>
-            
-            
-            
-            
-            
-
+<!-- begin menu stuff -->
+<ul id="maintab" class="basictab">
+    <li rel="homesubs"><a href="home.php"><img src="images/Home.jpg" onmouseover=this.src="images/Homeselected.jpg" 
+        onmouseout=this.src="images/Home.jpg" /></a></li>
+    <li rel="tradesubs"><a href="trading.php"><img src="images/Trade.jpg" onmouseover=this.src="images/Tradeselected.jpg" 
+        onmouseout=this.src="images/Trade.jpg" /></a></li>
+    <li rel="looksubs"><a href="lookup.php"><img src="images/Lookup.jpg" 
+        onmouseover=this.src="images/Lookupselected.jpg" onmouseout=this.src="images/Lookup.jpg" /></a></li>
+    <li rel="setsubs"><a href="settings.php"><img src="images/Settings.jpg" onmouseover=this.src="images/Settingsselected.jpg" 
+        onmouseout=this.src="images/Settings.jpg" /></a></li>
+    <li class="selected" rel="helpsubs"><a href="help.php"><img src="images/Help.jpg" 
+        onmouseover=this.src="images/Helpselected.jpg" onmouseout=this.src="images/Help.jpg" /></a></li>
+    <li rel="aboutsubs"><a href="about.php"><img src="images/About_Us.jpg" 
+        onmouseover=this.src="images/About_Usselected.jpg" onmouseout=this.src="images/About_Us.jpg" /></a></li>
+    <li rel="contactsubs"><a href="contact.php"><img src="images/Contact_Us.jpg" 
+        onmouseover=this.src="images/Contact_Usselected.jpg" onmouseout=this.src="images/Contact_Us.jpg" /></a></li>
 </ul>
 
-<div id="homesubs" class="submenustyle">
-
-</div>
-
+<div id="homesubs" class="submenustyle"></div>
 <div id="tradesubs" class="submenustyle">
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <a href="buy.php">Buy</a>
 <a href="sell.php">Sell</a>
 <a href="currentcash.php">Current Cash</a>
-<a href="tradehistory.php"><span style="color: rgb(255, 255, 0);">Trade History</span></a>
+<a href="tradehistory.php">Trade History</a>
 </div>
 
 <div id="looksubs" class="submenustyle">
@@ -100,12 +68,9 @@ onmouseover=this.src="images/Contact_Usselected.jpg" onmouseout=this.src="images
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <a href="newspg.php">NASDAQ News</a>
-
 </div>
 
-<div id="setsubs" class="submenustyle">
-
-</div>
+<div id="setsubs" class="submenustyle"></div>
 
 <div id="helpsubs" class="submenustyle">
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -118,79 +83,45 @@ onmouseover=this.src="images/Contact_Usselected.jpg" onmouseout=this.src="images
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <a href="faq.php"><img src="images/FAQ.jpg" onmouseover=this.src="images/FAQselected.jpg" 
-
-onmouseout=this.src="images/FAQ.jpg" /></a>
-<a href="glossary.php"><img src="images/Glossary.jpg" 
-
-onmouseover=this.src="images/Glossaryselected.jpg" onmouseout=this.src="images/Glossary.jpg" /></a>
+    onmouseout=this.src="images/FAQ.jpg" /></a>
+<a href="glossary.php"><img src="images/Glossaryselected.jpg" 
+    onmouseover=this.src="images/Glossaryselected.jpg" onmouseout=this.src="images/Glossaryselected.jpg" /></a>
 </div>
-
-<div id="aboutsubs" class="submenustyle">
-
-</div>
-
-<div id="contactsubs" class="submenustyle">
-
-</div>
-
+<div id="aboutsubs" class="submenustyle"></div>
+<div id="contactsubs" class="submenustyle"></div>
 
 <script type="text/javascript">
 //initialize tab menu, by passing in ID of UL
 initalizetab("maintab")
 </script>
 
-	<!-- end #menu -->
-	<div id="page">
-		<div id="page-bgtop">
-			<div id="page-bgbtm">
-				<div id="content">
-					
-<div class="post">
-						<h2 class="title"><a href="#">Trade History</a>			</h2>
-<p class="meta"><span class="posted"><a href="#"></a></span></p>
-						<div style="clear: both;">&nbsp;</div>
-						<div class="entry">
-						  <p>The Trade History page is under construction.<br />
-					      </p>
-<p class="links">&nbsp;</p>
-						</div>
-					</div>
-					<div class="post">
-						<h2 class="title">Another Cool Section</h2>
-						<p class="meta"><span class="date"> </span><span class="posted"> </span></p>
-						<div style="clear: both;">&nbsp;</div>
-						<div class="entry">
-							<p>&nbsp;</p>
-							<p class="links">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
-						</div>
-					</div>
-					<div style="clear: both;">&nbsp;</div>
-				</div>
-				<!-- end #content -->
-				<div id="sidebar">
-					<ul>
-                                            <li>
-							<h2>Trade History</h2>
-						</li>
-                                            <li></li>
-						<li>
-							<div id="search" >
-								<iframe id="datamain" src="announce.txt" width=284 height=300 marginwidth=10 marginheight=5 hspace=0 vspace=0 frameborder=1 scrolling=yes></iframe>
-							</div>
-							<div style="clear: both;">&nbsp;</div>
-						</li>
-                        <li></li>
-					</ul>
-	      </div>
-				<!-- end #sidebar -->
-				<div style="clear: both;">&nbsp;</div>
-			</div>
-		</div>
-	</div>
-	<!-- end #page -->
-</div>
+<!-- end #menu -->
+
+<?php  
+
+class TradeHistory
+{
+   public $numShares,$price,$symbol,$date,$userID;
+};
+function GetTradeHistory
+{
+   $history = new TradeHistory(
+   $req->history->numShares,               
+   $req->history->price,                  
+   $req->history->symbol,                
+   $req->history->transDate,             
+   $req->history->userID,     
+   );
+   $history->mysql_query("CALL getTradeHistory(userID)")
+      or die('Could not locate trade history: ' .mysql_error());
+   $token = $POST['token'];
+   $userID = mysql_query("call getUserID(token)");
+};
+   echo 'trade history php called';
+?>
+
 <div id="footer">
-	<p> &copy; 2012 Southern Oregon University 1250 Siskiyou Boulevard Ashland, OR 97520 541-552-7672</p>
+    <p> &copy; 2012 Southern Oregon University 1250 Siskiyou Boulevard Ashland, OR 97520 541-552-7672</p>
     <p> Webpages may include live content from third parties.</p>
     <p> Allergy Information:  Transmitted on a network that also transmits pictures of tree nuts.</p>
     <p> </p>
@@ -198,3 +129,5 @@ initalizetab("maintab")
 <!-- end #footer -->
 </body>
 </html>
+
+
