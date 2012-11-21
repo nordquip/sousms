415a90a64c66fa1e84e1d3e66d52c439f7570cdf
Hunter <blakelyh@sou.edu>
2012-11-21 11:05:18 -0800

    Update src/security/sanitize.php

--- src/security/sanitize.php
+++ src/security/sanitize.php
@@ -24,15 +24,16 @@ print ("Sanitize.php");
 //Username
 
 function checkName(){
+	// Username can contain any combination of lowercase/uppercase letters and #'s
 	$pattern ='[a-zA-Z0-9]*'; 
-	if(preg_match($pattern, $username))
-	{
+	$bool = preg_match($pattern, $username)
+	if($bool){
 		echo $username . " is an acceptable username.";
 	}
-	else
-	{
+	else{
 		echo $username . " is not an acceptable username.";
 	}
+	return $bool;
 }
 
 
@@ -48,22 +49,27 @@ function checkpassword(){
     		//mysql_real_escape_string, which prepends backslashes to the 
         	//following characters: \x00, \n, \r, \, ', " and \x1a.
         	$password = stripslashes(mysql_real_escape_string($password));
-        }
-	echo 'Input password ' . $p . ' gets changed to ' . $password;
+        	echo 'Input password ' . $p . ' gets changed to ' . $password;
+	}
+	
 }
 
 //Email
 // Check for an email address:
 function checkemail(){
+	$bool = false;
 	$pattern = '/[a-zA-Z0-9_-.+]+@[a-zA-Z0-9-]+.[a-zA-Z]+/';
 	if (empty($email)){
 		echo 'Please enter an email address.';
 	}
-        //checks to see if it is a proper token
-  	elseif(preg_match($pattern, $email)){
+	else{
+		$bool=preg_match($pattern, $email)
+	}
+  	elseif($bool){
   		echo $email . '\'s syntax is OK.'; 
 		$e = mysqli_real_escape_string($dbc, $email); // There is no $dbc variable, this won't work. ~HB
 	}
+	return $bool;
 }
 
 //Int
@@ -87,20 +93,21 @@ function checkint(){
 
 //Float
  function checkFloat(){
-    $pattern = '/^[0-9]\{0,\}[.]\{0,1\}[0-9]\{0,\}$/';
+	$bool = false;
+	$pattern = '/^[0-9]\{0,\}[.]\{0,1\}[0-9]\{0,\}$/';
 	if (empty($float)){
 		echo 'You forgot to enter your email address.';
 	}
 	else{
-		$x = preg_match($pattern, $float);
+		$bool = preg_match($pattern, $float);
 	}
-	if ($x){
+	if ($bool){
 		echo $float . " is float";
 	}
 	else{
 	  	echo $float . " Is not a float";
 	}
-	return $x;
+	return $bool;
 }
 
 
