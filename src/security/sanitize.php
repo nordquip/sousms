65d63c3d586cfa89bfd5cd0ab88b8f11d2c9dccc
Hunter <blakelyh@sou.edu>
2012-11-21 22:44:50 -0800

    Update src/security/sanitize.php

--- src/security/sanitize.php
+++ src/security/sanitize.php
@@ -7,116 +7,39 @@
 //Int
 //Float
 
-//vars from input
-$username = trim($_POST['username']);
-
-$password = trim($_POST['password']);
-
-$email = trim($_POST['email']);
-
-$int = trim($_POST['int']);
-
-$float = trim($_POST['float']);
-
-print ("Sanitize.php");
-
 
 //Username
-
-function checkName(){
-	// Username can contain any combination of lowercase/uppercase letters and #'s
-	$pattern ='[a-zA-Z0-9]*'; 
-	$bool = preg_match($pattern, $username)
-	if($bool){
-		echo $username . " is an acceptable username.";
-	}
-	else{
-		echo $username . " is not an acceptable username.";
-	}
-	return $bool;
+function checkName($username){
+	$pattern ='/[a-zA-Z0-9]*/'; 
+	return  preg_match($pattern, $username);
 }
 
 
 //Password
-function checkpassword(){             
-	$p = $password;
-	If (empty($password)){
-             echo 'Please enter a password.';
-    	}
-	else{
-        	//accept password entry and sanitize it
-        	//mysql_real_escape_string() calls MySQL's library function 
-    		//mysql_real_escape_string, which prepends backslashes to the 
-        	//following characters: \x00, \n, \r, \, ', " and \x1a.
-        	$password = stripslashes(mysql_real_escape_string($password));
-        	echo 'Input password ' . $p . ' gets changed to ' . $password;
-	}
-	
+function checkpassword($password){             
+	$pattern = '/[a-zA-Z0-9!@#$%^&*(),.-_=+\ ~`"?\{\}\[\]]*/';
+	return preg_match($pattern, $password);
 }
+
 
 //Email
-// Check for an email address:
-function checkemail(){
-	$bool = false;
-	$pattern = '/[a-zA-Z0-9_-.+]+@[a-zA-Z0-9-]+.[a-zA-Z]+/';
-	if (empty($email)){
-		echo 'Please enter an email address.';
-	}
-	else{
-		$bool=preg_match($pattern, $email)
-	}
-  	elseif($bool){
-  		echo $email . '\'s syntax is OK.'; 
-		$e = mysqli_real_escape_string($dbc, $email); // There is no $dbc variable, this won't work. ~HB
-	}
-	return $bool;
+function checkemail($email){
+	$pattern = '/[a-zA-Z0-9_-.+]@[a-zA-Z0-9-].[a-zA-Z]/';
+	return preg_match($pattern, $email);
 }
 
+
 //Int
-function checkint(){
-	$bool = false;
-	if (empty($int)){
-		echo 'Please enter an integer.';
-	}
-	else{
-		$bool = is_int($int);
-	}
-	if($bool){
-	  	echo $int . " is an int.";  	
-	}
-	else{
-	  	echo $int . " is not an int.";
-	}
-	return $bool;
+function checkint($int){
+	return is_int($int);
 }
 
 
 //Float
- function checkFloat(){
-	$bool = false;
+function checkFloat($float){
 	$pattern = '/^[0-9]\{0,\}[.]\{0,1\}[0-9]\{0,\}$/';
-	if (empty($float)){
-		echo 'You forgot to enter your email address.';
-	}
-	else{
-		$bool = preg_match($pattern, $float);
-	}
-	if ($bool){
-		echo $float . " is float";
-	}
-	else{
-	  	echo $float . " Is not a float";
-	}
-	return $bool;
+	return preg_match($pattern, $float);
 }
-
-
-//calls the functions
-checkName();
-checkpassword();
-checkemail();
-checkint();
-checkFloat();
 
 //EOF
 ?>
