d732d162f9cdf8ed3c68cfdd600735cf3ba230f5
Cody <shiltsc@sou.edu>
2012-11-28 15:45:14 -0800

    FinalProj

--- src/mobile/AppForSchool/org.example.webviewdemo.WebViewDemo/gen/org/example/webviewdemo/R.java
+++ src/mobile/AppForSchool/org.example.webviewdemo.WebViewDemo/gen/org/example/webviewdemo/R.java
@@ -14,11 +14,14 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int home_button=0x7f050000;
-        public static final int profile_button=0x7f050001;
-        public static final int research_button=0x7f050003;
-        public static final int trade_button=0x7f050002;
-        public static final int webview_compontent=0x7f050004;
+        public static final int home_button=0x7f050003;
+        public static final int ll_sub=0x7f050002;
+        public static final int profile_button=0x7f050004;
+        public static final int research_button=0x7f050006;
+        public static final int sl_sub=0x7f050000;
+        public static final int trade_button=0x7f050005;
+        public static final int webview_compontent=0x7f050001;
+        public static final int webview_compontent1=0x7f050007;
     }
     public static final class layout {
         public static final int main=0x7f030000;
