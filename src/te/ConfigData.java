e3648395e20e3e157d8462bcad8929eb12f0ad41
Nickolaus D.S <nick@incendiarysoftware.com>
2012-11-28 16:33:03 -0800

    Small refactor on looper config parser
    The config parser used to load config info in a specific order. It
    now loads all info regardless of order, as a real config parser should.

--- src/te/ConfigData.java
+++ src/te/ConfigData.java
@@ -1,52 +1,56 @@
 import java.io.*;
+import java.util.*;
 
 class ConfigData {
-    private String fileName;    
-    private final String[] fieldData = {
-        "mysqlUser",
-        "mysqlPassword",
-        "mysqlDatabase",
-        "mysqlHost"
-    };
-    private String[] valueData;
-    
+    private String fileName; 
+
+    //These array lists act much like a dictionary. Each index in both arrays
+    //is pared with the other. 
+    private ArrayList<String> configKeys;
+    private ArrayList<String> configValues;
     
     public ConfigData(String filename) {
         this.fileName = filename;
-        valueData = new String[fieldData.length];
+        configKeys = new ArrayList<String>();
+        configValues = new ArrayList<String>();
         getConfigData();
     }
     
     public String get(String field) {
         String retval = null;
         
-        for(int i = 0; i < fieldData.length; i++) {
-            if(fieldData[i].equals(field))
-                retval = valueData[i];
+        for(int i = 0; i < configKeys.size(); i++) {
+            if(configKeys.get(i).equals(field))
+                retval = configValues.get(i);
         }
         
         return retval;
     } 
     
     private void getConfigData() {
-        String input; //stores lines read from config file
+        String input = ""; //stores lines read from config file
         try {
             BufferedReader configFile = new BufferedReader(new FileReader(new File(fileName)));
             
             //Throw away everything until we reach the data section.
             while(!configFile.readLine().equals("<data>")) {}
             
-            for(int i = 0; i < fieldData.length; i++) {
+            while(!input.equals("</data>")) {
             
                 input = configFile.readLine();
-                String exp = "</??" + fieldData[i] + ">";
+                String exp = "<|</|>";
                 String[] arr = input.split(exp);
                 
-                if(arr.length < 2)
-                    System.err.println("[CRITICAL] Failed to load \'" + fieldData[i] + "\' from config file.");
-                else
-                    //Regex stores the match in [1]
-                    valueData[i] = arr[1];
+                //A bit hackish, but we don't want to parse the end of the config file.
+                if( input.equals("</data>") )
+                    continue; 
+                else if(arr.length < 3)
+                    System.err.println("[CRITICAL] Failed to load \'" + input + "\' from config file.");
+                else {
+                    //Regex stores the key in [1] and the value in [2]
+                    configKeys.add(arr[1]);
+                    configValues.add(arr[2]);
+                }
             }
             
             configFile.close();
