bba3f87b3b686dbd4389e9b6e8f5273077902a00
Nickolaus D.S <nick@incendiarysoftware.com>
2012-12-04 12:13:32 -0800

    Added a couple files to test config parser
    The config parser is for the Looper Trade Engine.
    
    Too bad this wasn't done earlier, the testing group would have had some good stuff to test.

---
+++ src/te/Refrence/testConfigParser/ConfigTest.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+/**
+Driver class intended to test the config file. Not for actual use.
+ * 
+*/
+class ConfigTest {
+
+
+
+
+    
+    public static void main(String args[]) {
+        String FILENAME = "configTest.xml";
+    
+        ConfigData cg = new ConfigData(FILENAME);
+        
+                
+    }
+
+    
+    
+}
